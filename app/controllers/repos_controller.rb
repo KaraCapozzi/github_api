45d2b2853d74dfa058610d308b484b90a1b28859
Kara Capozzi <karacapozzi@gmail.com>
2018-04-03 18:18:15 -0700

    display stargazers_count for top repos

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,9 +1,11 @@
 class ReposController < ApplicationController
 
-def index
-  @response = HTTParty.get('https://api.github.com/search/repositories?q=sort=stars&order=desc
-')
-  JSON.parse(@response.body)
-end
+  def index
+    response = HTTParty.get('https://api.github.com/search/repositories?q=stars:>=10000&sort=stars&order=desc')
+    parsed_response = JSON.parse(response.body)
+
+    @repos = parsed_response['items']
+
+  end
 
 end
