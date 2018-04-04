e339a544f8233114c487502b6cb9cc128bafa082
Kara Capozzi <karacapozzi@gmail.com>
2018-04-03 23:05:06 -0700

    top contributors

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,10 +1,13 @@
 class ReposController < ApplicationController
 
   def index
-    response = HTTParty.get('https://api.github.com/search/repositories?q=stars:>=10000&sort=stars&order=desc')
+    response = HTTParty.get('https://api.github.com/search/repositories?q=stars:>=10000&sort=stars&order=desc:closed&per_page=100')
     parsed_response = JSON.parse(response.body)
-
     @repos = parsed_response['items']
+
+    response_contributors = HTTParty.get('https://api.github.com/repos/ruby/ruby/contributors?q=contributions&order=desc')
+    parsed_response_contributors = JSON.parse(response_contributors.body)
+    @contributors = response_contributors[0]['login']
 
   end
 
